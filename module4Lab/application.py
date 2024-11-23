0c554564a69cc91092e147a9ae4ed38c91fb7e24
DieCamDu <dcamposd@ucenfotec.ac.cr>
2024-11-22 21:06:00 -0500

    Tenth commit Module 5 programming assignment Testing

--- module4Lab/application.py
+++ module4Lab/application.py
@@ -1,20 +1,21 @@
-from flask import Flask,request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-
 class Book(db.Model):
     id=db.Column(db.Integer, primary_key = True)
-    book_name=db.Column(db.String(80), unique=True, nullable=False)
+    title=db.Column(db.String(80), unique=True, nullable=False)
     author=db.Column(db.String(120))
     publisher=db.Column(db.String(50))
     
     def __repr__(self):
-        return f"{self.id} - {self.book_name} - {self.author} - {self.publisher}"
+        return f"{self.id} - {self.title} - {self.author} - {self.publisher}"
     
 
 @app.route('/')
@@ -27,7 +28,7 @@ def get_books():
     books = Book.query.all()
     output = []
     for book in books:
-        book_data = {'Id: ' : book.id, 'Name: ' : book.book_name, 'Author: ':book.author, 'Publisher: ':book.publisher}
+        book_data = {'Id: ' : book.id, 'Title: ' : book.title, 'Author: ':book.author, 'Publisher: ':book.publisher}
         output.append(book_data)
     return {"books": output}
 
@@ -36,21 +37,22 @@ def get_books():
 @app.route('/books/<id>')
 def get_book(id):
     book = Book.query.get_or_404(id)
-    return {'Name: ' : book.book_name, 'Author: ':book.author, 'Publisher: ':book.publisher}
+    return {'Title: ' : book.title, 'Author: ':book.author, 'Publisher: ':book.publisher}
 
 #Create
 @app.route('/books', methods=['POST'])
-def add_book():
-    book = Book(book_name=request.json['book_name'], author=request.json['author'], publisher=request.json['publisher'])
-    db.session.add(book)
+def create_book():
+    new_book = Book(title=request.json['title'], author=request.json['author'], publisher=request.json['publisher'])
+    db.session.add(new_book)
     db.session.commit()
-    return {'id:': book.id}
+    return jsonify({'message': 'Book added'}), 201
+
 
 #Delete
 @app.route('/books/<id>', methods=['DELETE'])
 def delete_book():
     book = Book.query.get(id)
-    if drink is None:
+    if book is None:
         return {"error": "not found"}
     db.session.delete(book)
     db.session.commit()
@@ -58,10 +60,14 @@ def delete_book():
 
 #Update
 @app.route('/books/<id>', methods=['PUT'])
-def update_book():
+def update_book(id):
     book = Book.query.get(id)
-    book.book_name = data.get('book_name', book.book_name)
+    book.title = data.get('title', book.title)
     book.author = data.get('author', book.author)
     book.publisher = data.get('publisher', book.publisher)
     
     db.session.commit()
+    
+if __name__ == "__main__":
+    db.create_all()  # Create the database tables
+    app.run(debug=True)
